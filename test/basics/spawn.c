5220319ea1aca58e0e32ab1f0226e6e0c3245240
ahori@riken.jp <ahori@wallaby10.aics-sys.riken.jp>
2016-11-02 14:27:43 +0900

    minor changes

--- test/basics/spawn.c
+++ test/basics/spawn.c
@@ -7,11 +7,13 @@
  * Written by Atsushi HORI <ahori@riken.jp>, 2016
  */
 
+#define DEBUG
 #include <test.h>
 
 int main( int argc, char **argv ) {
   int pipid;
 
+  DBG;
   TESTINT( pip_init( &pipid, NULL, NULL, 0 ) );
   if( pipid == PIP_PIPID_ROOT ) {
     ignore_signal( SIGINT );
@@ -20,19 +22,20 @@ int main( int argc, char **argv ) {
     TESTINT( pip_spawn( argv[0], argv, NULL, PIP_CPUCORE_ASIS, &pipid,
 			NULL, NULL, NULL ) );
     TESTINT( pip_wait( pipid, NULL ) );
-    TESTINT( pip_fin() );
 
   } else if( pipid < 10 ) {
     DBGF( "pipid=%d", pipid );
-
+    sleep( 2 );
     set_sigsegv_watcher();
     set_sigint_watcher();
 
-    pipid = PIP_PIPID_ANY;
+    pipid++;
     TESTINT( pip_spawn( argv[0], argv, NULL, PIP_CPUCORE_ASIS, &pipid,
 			NULL, NULL, NULL ) );
     TESTINT( pip_wait( pipid, NULL ) );
   }
+  DBG;
+  TESTINT( pip_fin() );
   DBG;
   return 0;
 }
