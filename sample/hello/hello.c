4e2485e879ef1d9467622d472ec24a9b50951815
ahori@riken.jp <ahori@wallaby9.aics-sys.riken.jp>
2016-11-24 08:12:15 +0000

    adding sample programs adding sbrk() test program

---
+++ sample/hello/hello.c
@@ -0,0 +1,32 @@
+#include <sys/wait.h>
+#include <stdio.h>
+#include <pip.h>
+
+#ifdef PRINT_MAPS
+#include <pip_debug.h>
+#endif
+
+int pipid, ntasks;
+
+int main( int argc, char **argv ) {
+
+  ntasks = 10;
+  pip_init( &pipid, &ntasks, NULL, 0 );
+
+  if( pipid == PIP_PIPID_ROOT ) { // PIP Root process
+    pipid = PIP_PIPID_ANY;
+    printf( "<R> &pipid: %p\n", &pipid );
+    pip_spawn( argv[0], argv, NULL, PIP_CPUCORE_ASIS, &pipid,
+	       NULL, NULL, NULL );
+    wait( NULL );
+
+  } else {			// PIP child task
+#ifdef PRINT_MAPS
+    pip_print_maps();
+#endif
+    printf( "<%d> &pipid: %p\n", pipid, &pipid );
+    printf( "<%d> Hello, I am fine !!\n", pipid );
+  }
+  pip_fin();
+  return 0;
+}
