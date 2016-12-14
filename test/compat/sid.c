816881b70f6d9f8b03f3fe4110a4e300bc985f33
ahori@riken.jp <ahori@wallaby9.aics-sys.riken.jp>
2016-12-14 11:47:52 +0900

    1) Adding setsid() and getsid() test 2) Remove profiling define symbol in pip.c

---
+++ test/compat/sid.c
@@ -0,0 +1,39 @@
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include <pip.h>
+
+#define NTASKS	(10)
+
+int main( int argc, char **argv ) {
+  pid_t sid_old, sid_new;
+  int  i, ntasks, pipid;
+
+  ntasks = NTASKS;
+
+  sid_old = getsid( getpid() );
+
+  pip_init( &pipid, &ntasks, NULL, 0 );
+  if( pipid == PIP_PIPID_ROOT ) {
+    if( ( sid_new = setsid() ) < 0 ) {
+      printf( "ROOT: setsid(): %d\n", errno );
+    }
+    printf( "ROOT[%d]: sid %d -> %d\n", getpid(), sid_old, sid_new );
+    for( i=0; i<ntasks; i++ ) {
+      pipid = i;
+      pip_spawn( argv[0], argv, NULL, i, &pipid, NULL, NULL, NULL );
+    }
+    for( i=0; i<ntasks; i++ ) wait( NULL );
+    printf( "all done\n" );
+
+  } else {	/* PIP child task */
+    if( ( sid_new = setsid() ) < 0 ) {
+      printf( "CHILD: setsid(): %d\n", errno );
+    }
+    printf( "CHILD[%d]: sid %d -> %d\n", getpid(), sid_old, sid_new );
+  }
+  pip_fin();
+  return 0;
+}
