8511e5ab0f1c247e8075f0ba4910572669f09f09
SODA Noriyuki <soda@sra.co.jp>
2016-12-20 13:30:58 +0900

    make "make check" work with recent PIP-glibc since commit:pip-glibc|785f9dab7f935a43290702f12e1b39f5575d4d09
    except spawns.sh and fort.sh

--- test/util/dlmopen_count.c
+++ test/util/dlmopen_count.c
@@ -1,14 +1,30 @@
 #define _GNU_SOURCE
 #include <dlfcn.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define MAX 1000
+#include <pip.h>
+
+#define MAX 1024
 
 int
 main(int argc, char **argv)
 {
 	void *so;
-	int n = 0;
+	int option_pip_mode = 1;
+	int c, n = 0;
+
+	while ((c = getopt(argc, argv, "p")) != -1) {
+		switch (c) {
+		case 'p':
+			option_pip_mode = 1;
+			break;
+		default:
+			fprintf(stderr, "Usage: dlmopen_count [-p]\n");
+			exit(2);
+		}
+	}
 
 	for (;;) {
 		so = dlmopen(LM_ID_NEWLM, argv[0], RTLD_NOW | RTLD_LOCAL);
@@ -17,6 +33,8 @@ main(int argc, char **argv)
 		if (++n >= MAX)
 			break;
 	}
+	if (option_pip_mode && n > PIP_NTASKS_MAX)
+		n = PIP_NTASKS_MAX;
 	printf("%d\n", n);
 	return 0;	
 }
