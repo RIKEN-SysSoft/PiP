cac9ed82d09f8723155b73484c0949f8f756f37a
ahori@riken.jp <ahori@riken.jp>
2017-01-09 13:38:02 +0900

    Adding utility prog which prints mmap

---
+++ bin/pmap.c
@@ -0,0 +1,18 @@
+/*
+ * $RIKEN_copyright:$
+ * $PIP_VERSION:$
+ * $PIP_license:$
+ */
+/*
+ * Written by Atsushi HORI <ahori@riken.jp>, 2016
+ */
+
+#include <sys/wait.h>
+#include <stdio.h>
+
+#include <pip_debug.h>
+
+int main() {
+  pip_print_maps();
+  return 0;
+}
