c19fa0ea521f05618756be39d7dd80537df1e2d5
ahori@riken.jp <ahori@riken.jp>
2017-01-18 11:24:55 +0900

    Adding mmap2 evaluation

---
+++ eval/mmap2/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/sh
+
+date
+cat $0
+
+PROGS="mmap-pip mmap-thread mmap-forkonly"
+NTASKS=15
+
+doeval() {
+    if [ $1 == mmap-pip ]
+    then export LD_PRELOAD=/home/ahori/work/PIP/PIP/preload/pip_preload.so
+    else unset LD_PRELOAD
+    fi
+    echo $1 $2 $3 LD_PRELOAD=${LD_PRELOAD}
+    numactl -C 0 ./$1 $2 $3
+}
+
+for NTASKS in 10 20 30 40 50 60
+do
+for PROG in ${PROGS}
+do
+for ITER in 1 2 3 4 5 6 7 8 9 10 11 12
+do
+doeval ${PROG} ${NTASKS} 32
+done
+done
+done
