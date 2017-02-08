56e6c079177c64b484c2064a781fd2295017246e
SODA Noriyuki <soda@sra.co.jp>
2017-02-08 13:15:25 +0900

    reflect commit:pip|commit ffa61704aa1a33f0fe74b05d17e0274fd75fa732

--- test/compat/brk.sh
+++ test/compat/brk.sh
@@ -8,7 +8,7 @@ trap 'rm -f $TEST_TMP; exit $EXIT_KILLED' $TEST_TRAP_SIGS
 if [ $(grep '^<.* brk:' <$TEST_TMP | wc -l) \
 	-eq $(expr $TEST_PIP_TASKS + 1) ] &&
    [ $(awk '$0 ~ /^<.* brk:/ { print $2, $3}' $TEST_TMP | sort -u | wc -l) \
-	-eq 1 ]
+	-eq $(expr $TEST_PIP_TASKS + 1) ]
 then
 	test_exit_status=$EXIT_PASS
 fi
