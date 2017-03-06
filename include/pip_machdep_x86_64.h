6bdabb4dbe4687a1427fb4cf2e03e7e18a2f58ce
SODA Noriyuki <soda@sra.co.jp>
2017-03-06 11:02:49 +0900

    use gcc built-in atomic functions, suggested by hori-san

--- include/pip_machdep_x86_64.h
+++ include/pip_machdep_x86_64.h
@@ -27,6 +27,7 @@ inline static void pip_pause( void ) {
 		"delay %eax;" );
 #endif
 }
+#define PIP_PAUSE
 
 /* come from spin_lock() on glibc 2.3.3 (Fedora Core 1) */
 
@@ -39,11 +40,7 @@ inline static int pip_spin_trylock (pip_spinlock_t *lock) {
      : "0" (0));
   return oldval > 0; /* Change return value to kernel function by Kameyama */
 }
-
-inline static int pip_spin_lock (pip_spinlock_t *lock) {
-  while( !pip_spin_trylock( lock ) ) pip_pause();
-  return 0;
-}
+#define PIP_SPIN_TRYLOCK
 
 inline static int pip_spin_unlock (pip_spinlock_t *lock) {
   asm volatile
@@ -51,24 +48,17 @@ inline static int pip_spin_unlock (pip_spinlock_t *lock) {
      : "=m" (*lock));
   return 0;
 }
-
-inline static int pip_spin_init (pip_spinlock_t *lock) {
-  pip_spin_unlock( lock );
-  return 0;
-}
-
-inline static int pip_spin_destroy (pip_spinlock_t *lock) {
-  /* Nothing to do.  */
-  return 0;
-}
+#define PIP_SPIN_UNLOCK
 
 inline static void pip_write_barrier(void) {
   asm volatile("sfence":::"memory");
 }
+#define PIP_WRITE_BARRIER
 
 inline static void pip_memory_barrier(void) {
   asm volatile("mfence":::"memory");
 }
+#define PIP_MEMORY_BARRIER
 
 #endif
 
