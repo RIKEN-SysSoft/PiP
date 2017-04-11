9f8dac0194911a075399d77b76c3594d26ec006e
SODA Noriyuki <soda@sra.co.jp>
2017-04-11 11:31:39 +0900

    PIP_MODE_PROCESS is specified in test/basics/file.c

--- test/basics/file.sh
+++ test/basics/file.sh
@@ -2,4 +2,9 @@
 
 . ../test.sh.inc
 
+# PIP_MODE_PROCESS is specified in pip_init()
+case `../util/pip_mode` in
+pthread) exit $EXIT_UNSUPPORTED;;
+esac
+
 ./file 2>&1 | test_msg_count 'checking -- ok' $(expr $TEST_PIP_TASKS + 1)
