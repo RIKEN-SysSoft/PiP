da7548ef5e0f1865c4519826f441425f21631714
SODA Noriyuki <soda@sra.co.jp>
2017-04-11 11:43:09 +0900

    make test/basics/stack return UNSUPPORTED if $PIP_MODE is process:pipclone or pthread

--- test/basics/stack.sh
+++ test/basics/stack.sh
@@ -9,5 +9,14 @@ if [ $(grep ' stack=' <$TEST_TMP | wc -l) -eq $TEST_PIP_TASKS ]
 then
 	test_exit_status=$EXIT_PASS
 fi
+
+case `../util/pip_mode` in
+process:pipclone|pthread)
+	case $test_exit_status in
+	$EXIT_PASS)	test_exit_status=$EXIT_XPASS;;
+	$EXIT_FAIL)	test_exit_status=$EXIT_UNSUPPORTED;;
+	esac
+esac
+
 rm -f $TEST_TMP
 exit $test_exit_status
