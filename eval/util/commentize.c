388293f9bed5481ba04591369ad76e9f121aadb5
ahori@riken.jp <ahori@riken.jp>
2017-07-04 13:35:34 +0900

    Add utility prog

---
+++ eval/util/commentize.c
@@ -0,0 +1,31 @@
+/*
+ * $RIKEN_copyright:$
+ * $PIP_VERSION:$
+ * $PIP_license:$
+ */
+/*
+ * Written by Atsushi HORI <ahori@riken.jp>, 2016
+ */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+
+int main( int argc, char **argv ) {
+  FILE *fp;
+  char *line = NULL;
+  size_t n = 0;
+
+  if( argv[1] == NULL ) {
+    fprintf( stderr, "filename must be specified\n" );
+    return 1;
+  } else if( ( fp = fopen( argv[1], "r" ) ) == NULL ) {
+    fprintf( stderr, "Unable to open '%s'\n", argv[1] );
+    return 1;
+  } else {
+    while( getline( &line, &n, fp ) >= 0 ) {
+      printf( "# %s", line );
+    }
+    fclose( fp );
+  }
+  return 0;
+}
