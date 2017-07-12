bf3ac3233a5843d5818d18adeb91dce0eeb0f446
SODA Noriyuki <soda@sra.co.jp>
2017-07-12 20:01:19 +0900

    make test programs independent from number of CPU cores

--- test/compat/brk.c
+++ test/compat/brk.c
@@ -27,8 +27,13 @@ int main( int argc, char **argv ) {
   if( pipid == PIP_PIPID_ROOT ) {
     for( i=0; i<NTASKS; i++ ) {
       pipid = i;
-      err = pip_spawn( argv[0], argv, NULL, i%4, &pipid, NULL, NULL, NULL );
-      if( err ) break;
+      err = pip_spawn( argv[0], argv, NULL, i % cpu_num_limit(),
+		       &pipid, NULL, NULL, NULL );
+      if( err ) {
+	fprintf( stderr, "pip_spawn(%d/%d): %s\n",
+		 i, NTASKS, strerror( err ) );
+	break;
+      }
       if( i != pipid ) {
 	fprintf( stderr, "pip_spawn(%d!=%d)=%d !!!!!!\n", i, pipid, err );
 	break;
