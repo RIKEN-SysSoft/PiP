8e5ab68baf47990d82486dc09e5e55c73842ff51
g10013 <g10013@c7943.ofp>
2017-07-31 11:07:36 +0900

    change size (4096->8192)

---
+++ eval/stencil/perf.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+. ../eval.sh.inc
+
+PROGS="stencil_pip stencil_mpi"
+FPROGS="stencil_pip_fast stencil_mpi_fast"
+#SIZE=4096
+SIZE=8192
+ENERGY=1
+NITERS=1000
+FAST=1
+PIPRUN=`pwd`/../../bin/piprun
+MODES="process"
+
+doperf() {
+    for ITER in 1 2 3 4 5 6 7 8 9 10 11 12
+    do
+	if [ $1 == stencil_pip_fast ]
+	then
+	    PIPMODE=process
+	    export PIP_MODE=$PIPMODE;
+	    echo -n "["$ITER"]" $1:$PIPMODE $2 "(" $3 ") "
+	    $PIPRUN -b -n $2 ./$1 $SIZE $ENERGY $3
+	else
+	    echo -n "["$ITER"]" $1 $2 "(" $3 ") "
+	    mpirun -np $2 ./$1 $SIZE $ENERGY $3
+	fi
+    done
+}
+
+csv_begin perf
+
+for LCOUNT in 2 30 100 300 1000 3000 10000 
+do
+    for NTASKS in 4 8 16 32 64
+    do
+	if [ $NTASKS -gt $NTMAX ]
+	then
+	    break
+	fi
+	for PROG in ${FPROGS}
+	do
+	    doperf ${PROG} ${NTASKS} ${LCOUNT}
+	done
+    done
+done
+
+csv_end perf
