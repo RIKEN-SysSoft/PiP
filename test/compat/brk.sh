bc2f430d9d2ceca8a70f663b59b4207917366b88
ahori@riken.jp <ahori@riken.jp>
2017-08-09 17:43:27 +0900

    merge

--- test/compat/brk.sh
+++ test/compat/brk.sh
@@ -5,8 +5,8 @@
 trap 'rm -f $TEST_TMP; exit $EXIT_KILLED' $TEST_TRAP_SIGS
 
 # the "exit 0"  below is to prevent the followin abort:
-#	brk.sh: line 9: <PID> Segmentation fault      (core dumped) 
-( ./brk; exit 0 ) >$TEST_TMP 2>&1
+#	brk.sh: line 9: <PID> Segmentation fault      (core dumped)
+( $MCEXEC ./brk; exit 0 ) >$TEST_TMP 2>&1
 if [ $(grep '^<.* brk:' <$TEST_TMP | wc -l) \
 	-eq $(expr $TEST_PIP_TASKS + 1) ] &&
    [ $(awk '$0 ~ /^<.* brk:/ { print $2, $3}' $TEST_TMP | sort -u | wc -l) \
