0b05982a8bd7d8d281f034d4122e584c055e1ce3
Atsushi Hori <ahori@riken.jp>
2017-08-28 15:26:09 +0900

    tutorial programs

---
+++ tutorial/prgs/hello-mpi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main( int argc, char** argv ) {
+  int rank;
+
+  MPI_Init( &argc, &argv );
+  MPI_Comm_rank( MPI_COMM_WORLD, &rank );
+  printf( "Hello from MPI process %d", rank );
+  MPI_Finalize();
+  return 0;
+}
