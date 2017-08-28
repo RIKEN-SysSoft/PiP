bf0f78ef241a29f24f647b432578ec94c4f2ef64
ahori@riken.jp <ahori@riken.jp>
2017-08-28 16:31:57 +0900

    Adding vars programs

---
+++ tutorial/prgs/vars0-omp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <omp.h>
+
+int gvar = 0;
+
+int main( int argc, char **argv ) {
+  int lvar = 0;
+  int tid;
+
+#pragma omp parallel private(tid)
+  {
+    tid = omp_get_thread_num();
+    printf( "[%d] gvar:%p lvar:%p tid:%p\n", tid, &gvar, &lvar, &tid );
+  }
+  return 0;
+}
