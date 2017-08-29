400493c404444edec6cb8909d7646bc6f090897b
ahori@riken.jp <ahori@riken.jp>
2017-08-29 10:45:59 +0900

    Adding and modifying tutorial programs

--- tutorial/prgs/vars0-omp.c
+++ tutorial/prgs/vars0-omp.c
@@ -10,7 +10,7 @@ int main( int argc, char **argv ) {
 #pragma omp parallel private(tid)
   {
     tid = omp_get_thread_num();
-    printf( "[%d] gvar:%p lvar:%p tid:%p\n", tid, &gvar, &lvar, &tid );
+    printf( "<%d> gvar:%p lvar:%p tid:%p\n", tid, &gvar, &lvar );
   }
   return 0;
 }
--- tutorial/prgs/vars0-pip.c
+++ tutorial/prgs/vars0-pip.c
@@ -8,7 +8,7 @@ int main( int argc, char **argv ) {
   int pipid;
 
   pip_init( &pipid, NULL, NULL, 0 );
-  printf( "[%d] gvar:%p lvar:%p\n", pipid, &gvar, &lvar );
+  printf( "<%d> gvar:%p lvar:%p\n", pipid, &gvar, &lvar );
   pip_fin();
   return 0;
 }
