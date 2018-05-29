cd0e32a2ddbd2bc52cb09c63163b5e76fbb1b895
ahori@riken.jp <ahori@riken.jp>
2018-05-29 17:44:27 +0900

    1) minor change 2) ulp test programs are renamed

--- test/ulp/migrate.c
+++ test/ulp/migrate.c
@@ -10,8 +10,7 @@
 #define PIP_INTERNAL_FUNCS
 
 //#define NULPS	(10)
-#define NULPS	(10)
-//#define NULPS	(0)
+#define NULPS	(3)
 
 #define DEBUG
 #include <test.h>
@@ -28,11 +27,12 @@ int main( int argc, char **argv ) {
 
   //fprintf( stderr, "PID %d\n", getpid() );
 
-  //ntasks = NULPS*2;
-  ntasks = 100;
+  ntasks = NULPS*2;
   TESTINT( pip_init( &pipid, &ntasks, NULL, 0 ) );
   if( pipid == PIP_PIPID_ROOT ) {
-    pip_ulp_t *ulp  = NULL;
+    pip_spawn_program_t prog;
+
+    pip_ulp_t *ulp = NULL;
     root.pipid = pipid;
     for( i=0; i<NULPS; i++ ) {
       pipid = i + 1;
@@ -43,14 +43,14 @@ int main( int argc, char **argv ) {
 			    ulp,
 			    &ulp ) );
     }
+    pip_spawn_from_main( &prog, argv[0], argv, NULL );
     pipid = 0;
-    TESTINT( pip_spawn_with_ulps( argv[0], argv, NULL, 0, &pipid,
-				  NULL, NULL, NULL, ulp ) );
+    TESTINT( pip_task_spawn_with_ulps( &prog, PIP_CPUCORE_ASIS, &pipid, ulp ));
     TESTINT( pip_wait( pipid, NULL ) );
   } else {
-    fprintf( stderr, "\n<%d> Hello from ULP (stackvar@%p staticvar@%p)\n\n",
+    fprintf( stderr, "\n<%d> Hello, ULP (stackvar@%p staticvar@%p)\n\n",
 	     pipid, &pipid, &root );
-    if( pipid == 0 ) pip_ulp_retire( pipid );
+    pip_exit( 0 );
   }
   TESTINT( pip_fin() );
   return 0;
