82d75aa5b11a6eb8aec4d4f2c3be25027e03a9b8
SODA Noriyuki <soda@sra.co.jp>
2018-08-13 17:37:23 +0900

    apply the file-vs-pipe-redirection workaround (#508) to test/basics/stack.sh too
    this is similar workaround to test/spawn/spawns.sh
    in commit:pip|ab34fdccde4f2006679c255c30737b954f090c4c for Ubuntu-12.04

--- test/basics/stack.sh
+++ test/basics/stack.sh
@@ -4,7 +4,9 @@
 
 trap 'rm -f $TEST_TMP; exit $EXIT_KILLED' $TEST_TRAP_SIGS
 
-$MCEXEC ./stack >$TEST_TMP 2>&1
+# redmine #508
+#$MCEXEC ./stack >$TEST_TMP 2>&1
+$MCEXEC ./stack 2>&1 | tee $TEST_TMP >/dev/null
 if [ $(grep ' stack=' <$TEST_TMP | wc -l) -eq $TEST_PIP_TASKS ]
 then
 	test_exit_status=$EXIT_PASS
@@ -14,7 +16,7 @@ case `../util/pip_mode` in
 process:pipclone|pthread)
 	case $test_exit_status in
 	$EXIT_PASS)	test_exit_status=$EXIT_PASS;;
-	$EXIT_FAIL)	test_exit_status=$EXIT_UNSUPPORTED;;
+	$EXIT_FAIL)	test_exit_status=$EXIT_XFAIL;; # redmine #508
 	esac
 esac
 
