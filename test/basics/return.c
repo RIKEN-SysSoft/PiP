0dc7a303221caa7b907537a96cfc622463dbaf4c
ahori@riken.jp <ahori@riken.jp>
2019-06-28 15:06:32 +0900

    forgot tyo add

--- test/basics/return.c
+++ test/basics/return.c
@@ -33,62 +33,9 @@
   * Written by Atsushi HORI <ahori@riken.jp>, 2016
 */
 
-//#define DEBUG
 #include <test.h>
-#include <math.h>
 
-
-#define BIAS	(1000)
-
-int *a;
-
-int test_main( exp_t *exp ) {
-  int pipid, ntasks = exp->args.ntasks;
-  int *ap, flag, i, k, n;
-
-  if( ntasks < 2 ) return 1;
-
-  if( pip_is_debug_build() ) {
-    n = 20;
-  } else {
-    n = 5;
-  }
-  a = (int*) malloc( sizeof(int) * n );
-
-  flag = 0;
-  TESTINT( pip_get_pipid( &pipid ) );
-  srand( pipid );
-  for( i=0; i<n; i++ ) {
-    if( pipid & 0x1 ) {
-      a[i] = ( BIAS * pipid ) + i;
-    } else {
-      a[i] = -1;;
-    }
-  }
-
-  for( i=0; i<n; i++ ) {
-    if( pipid & 0x1 ) {
-      ap = &a[i];
-      fprintf( stderr, "[%d] export[%d]\n", pipid, i );
-      TESTINT( pip_named_export( (void*) ap, "a[%d]", i ) );
-    } else {
-      do {
-	k = rand() % ntasks;
-      } while( !( k & 0x1 ) );
-      fprintf( stderr, "[%d] import[%d]@%d\n", pipid, i, k );
-      TESTINT( pip_named_import( k, (void**) &ap, "a[%d]", i ) );
-      if( *ap != ( BIAS * k ) + i ) {
-	flag = 1;
-	fprintf( stderr, "[%d] i=%d  %d != %d\n", pipid, i, *ap, i+BIAS );
-      }
-    }
-    if( pipid == 0 && i % 10 == 0 ) {
-      fprintf( stderr, "ITER:%d\n", i );
-    }
-  }
-#ifdef DEBUG
-  fprintf( stderr, "\n[pipid:%d] DONE\n\n", pipid );
-#endif
-  TESTINT( pip_barrier_wait( &exp->pbarr ) );
-  return flag;
+int main() {
+  set_sigsegv_watcher();
+  return 0;			/* dummy */
 }
