ccaf6fa778ea2fe5a2760865c047d0f6cc5184a6
ahori@riken.jp <ahori@riken.jp>
2019-06-28 15:07:14 +0900

    forgot to add

---
+++ test/pthread/pthread_barrier.c
@@ -0,0 +1,81 @@
+/*
+ * $RIKEN_copyright: 2018 Riken Center for Computational Sceience,
+ * 	  System Software Devlopment Team. All rights researved$
+ * $PIP_VERSION: Version 1.0$
+ * $PIP_license: <Simplified BSD License>
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * official policies, either expressed or implied, of the PiP project.$
+ */
+/*
+ * Written by Atsushi HORI <ahori@riken.jp>, 2016
+ */
+
+#include <test.h>
+
+#define NITERS		(100)
+#define NTHREADS	(100)
+
+int		  niters;
+pthread_barrier_t barr;
+
+void *thread_main( void *argp ) {
+  int i;
+  for( i=0; i<niters; i++ ) {
+    CHECK( pthread_barrier_wait( &barr ),
+	   ( RV!=PTHREAD_BARRIER_SERIAL_THREAD && RV!=0 ),
+	   exit(EXIT_FAIL) );
+  }
+  return NULL;
+}
+
+int main( int argc, char **argv ) {
+  pthread_t threads[NTHREADS];
+  int nthreads;
+  int i;
+
+  if( argc > 1 ) {
+    nthreads = strtol( argv[1], NULL, 10 );
+  }
+  nthreads = ( nthreads == 0 ) ? NTHREADS : NITERS;
+
+  if( argc > 2 ) {
+    niters = strtol( argv[2], NULL, 10 );
+  }
+  niters = ( niters == 0 ) ? NITERS : niters;
+
+  CHECK( pthread_barrier_init( &barr, NULL, nthreads ),
+	 RV,
+	 return(EXIT_FAIL) );
+
+  for( i=0; i<nthreads; i++ ) {
+    CHECK( pthread_create( &threads[i], NULL, thread_main, NULL ),
+	   RV, return(EXIT_FAIL) );
+  }
+  for( i=0; i<nthreads; i++ ) {
+    CHECK( pthread_join( threads[i], NULL ), RV, return(EXIT_FAIL) );
+  }
+  return 0;
+}
