4fe88eb6bea559fea9be65689b45f11ef39dc2a2
SODA Noriyuki <soda@sra.co.jp>
2019-07-30 17:28:47 +0900

    revert recent changes against test/errno/pip_spawn.c
    commit:pip|15684f8bc37b598f380589e98906ae424cc9d295 was correct.

--- test/errno/pip_spawn.c
+++ test/errno/pip_spawn.c
@@ -8,7 +8,7 @@
 #include <test.h>
 
 int main( int argc, char **argv ) {
-  int pipid, rv, status, extval;
+  int pipid, rv;
 
   pipid = PIP_PIPID_ANY;
   rv = pip_spawn( argv[0], argv, NULL, PIP_CPUCORE_ASIS, &pipid,
@@ -17,23 +17,6 @@ int main( int argc, char **argv ) {
       fprintf( stderr, "pip_spawn: %s", strerror( rv ) );
       return EXIT_FAIL;
   }
-  status = 0;
-  rv = pip_wait( pipid, &status );
-  if( rv != 0 ) {
-    fprintf( stderr, "pip_wait: %s", strerror( rv ) );
-    return EXIT_FAIL;
-  }
-  if( WIFEXITED( status ) ) {
-    extval = WEXITSTATUS( status );
-  } else {
-    extval = EXIT_UNRESOLVED;
-  }
 
-  rv = pip_fin();
-  if( rv != 0 ) {
-    fprintf( stderr, "pip_fin: %s", strerror( rv ) );
-    return EXIT_FAIL;
-  }
-
-  return extval;
+  return EXIT_PASS;
 }
