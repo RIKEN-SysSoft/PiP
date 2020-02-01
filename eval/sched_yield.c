a92454051db333743407ec289162908bd87962f3
Atsushi Hori <ahori@riken.jp>
2020-02-01 12:28:44 +0900

    fix another bug

--- eval/sched_yield.c
+++ eval/sched_yield.c
@@ -26,9 +26,35 @@ double pip_gettime( void ) {
   return ((double)tv.tv_sec + (((double)tv.tv_usec) * 1.0e-6));
 }
 
-void *foo( void *dummy ) {
+void *foo1( void *dummy ) {
+  cpu_set_t	cpuset;
   int i, j;
-  
+
+  CPU_ZERO( &cpuset );
+  CPU_SET( 1, &cpuset );
+  pthread_setaffinity_np( pthread_self(), sizeof(cpuset), &cpuset );
+
+  pthread_barrier_wait( &barr );
+  for( j=0; j<NSAMPLES; j++ ) {
+    for ( i=0; i<witers; i++ ) {
+      (void) pthread_yield();
+    }
+    pthread_barrier_wait( &barr );
+    for ( i=0; i<niters; i++ ) {
+      (void) pthread_yield();
+    }
+    pthread_barrier_wait( &barr );
+  }
+}
+
+void *foo2( void *dummy ) {
+  cpu_set_t	cpuset;
+  int i, j;
+
+  CPU_ZERO( &cpuset );
+  CPU_SET( 2, &cpuset );
+  pthread_setaffinity_np( pthread_self(), sizeof(cpuset), &cpuset );
+
   pthread_barrier_wait( &barr );
   for( j=0; j<NSAMPLES; j++ ) {
     for ( i=0; i<witers; i++ ) {
@@ -46,22 +72,11 @@ int main() {
   double 	t, ts0[NSAMPLES], ts1[NSAMPLES];
   int		i, j;
   pthread_t	th[2];
-  pthread_attr_t attr[2];
-  cpu_set_t	cpuset[2];
-
-  pthread_attr_init( &(attr[0]) );
-  CPU_ZERO( &(cpuset[0]) );
-  CPU_SET( 1, &(cpuset[0]) );
-  pthread_attr_setaffinity_np( &(attr[0]), sizeof(cpuset), &(cpuset[0]) );
-
-  pthread_attr_init( &(attr[1]) );
-  CPU_ZERO( &(cpuset[1]) );
-  CPU_SET( 2, &(cpuset[1]) );
-  pthread_attr_setaffinity_np( &(attr[1]), sizeof(cpuset), &(cpuset[1]) );
+  pthread_attr_t attr;
 
   pthread_barrier_init( &barr, NULL, 3 );
-  pthread_create( &(th[0]), &(attr[0]), foo, NULL );
-  pthread_create( &(th[1]), &(attr[1]), foo, NULL );
+  pthread_create( &(th[0]), NULL, foo1, NULL );
+  pthread_create( &(th[1]), NULL, foo1, NULL );
   pthread_barrier_wait( &barr );
   for( j=0; j<NSAMPLES; j++ ) {  
     for( i=0; i<witers; i++ ) {
@@ -76,8 +91,8 @@ int main() {
   pthread_join( th[1], NULL );
 
   pthread_barrier_init( &barr, NULL, 3 );
-  pthread_create( &(th[0]), &(attr[0]), foo, NULL );
-  pthread_create( &(th[1]), &(attr[0]), foo, NULL );
+  pthread_create( &(th[0]), NULL, foo1, NULL );
+  pthread_create( &(th[1]), NULL, foo2, NULL );
   pthread_barrier_wait( &barr );
   for( j=0; j<NSAMPLES; j++ ) {  
     for( i=0; i<witers; i++ ) {
