ba06549189f06b79ac0996b50f5ad6b5c0457df6
SODA Noriyuki <soda@sra.co.jp>
2020-02-13 01:44:54 +0900

    specify $(glibc_libdir) as rpath to fix a problem that piprun dumps core
    with RPM package, the location of libpip.so is /usr/lib,
    and piprun loads stock glibc instead of PIP-glibc without this change.

--- build/var.mk
+++ build/var.mk
@@ -44,7 +44,9 @@ PIPINCDIR = $(top_srcdir)/include
 PIPLIBDIR = $(top_builddir)/lib
 PIPLIB = $(top_builddir)/lib/libpip.so
 
-PIPLDLIB = -L$(top_builddir)/lib -Wl,-rpath=$(libdir) -lpip
+PIPLDLIB = -L$(top_builddir)/lib \
+	-Wl,-rpath=$(glibc_libdir) -L$(glibc_libdir) \
+	-Wl,-rpath=$(libdir) -lpip
 
 PIPLDFLAGS = -pthread -pie -rdynamic -Wl,--dynamic-linker=$(dynamic_linker) \
 	$(PIPLDLIB) -ldl $(PIEFLAG)
