416975ef6db8d66aa77ad0319838c46c336c3d88
ahori@riken.jp <ahori@riken.jp>
2020-03-06 12:50:25 +0900

    Freeing patch tab

--- lib/pip_got.c
+++ lib/pip_got.c
@@ -80,6 +80,8 @@ static void pip_undo_got_patch( void ) {
     void *old_addr   = pip_got_patch_list[i].old_addr;
     *got_entry = old_addr;
   }
+  free( pip_got_patch_list );
+  pip_got_patch_size  = 0;
   pip_got_patch_currp = 0;
 }
 
