066664c01cb3b93ea49d6336c7eb4ee7021d0b81
ahori@riken.jp <ahori@riken.jp>
2020-03-20 17:10:02 +0900

    forgotten

--- include/pip_gdbif_enums.h
+++ include/pip_gdbif_enums.h
@@ -33,59 +33,38 @@
   * Written by Atsushi HORI <ahori@riken.jp>
 */
 
-#ifndef _pip_gdbif_h_
-#define _pip_gdbif_h_
+#ifndef _pip_gdbif_enums_h_
+#define _pip_gdbif_enums_h_
 
-#include <pip_machdep.h> /* for pip_spinlock_t */
-#include <pip_gdbif_queue.h>
-#include <pip_gdbif_enums.h>
-
-struct pip_gdbif_task {
-  /* double linked list */
-  /* assuming GDB only dereferencing the next pointer */
-  PIP_HCIRCLEQ_ENTRY(pip_gdbif_task) task_list;
-  struct pip_gdbif_task *root;
-  PIP_SLIST_ENTRY(pip_gdbif_task) free_list;
-  /* pathname of the program */
-  char	*pathname;
-  char	*realpathname;
-  /* argc, argv and env */
-  int	argc;
-  char 	**argv;
-  char 	**envv;
-  /* handle of the loaded link map */
-  void	*handle;
-  void	*load_address;
-  /* PID or TID of the PiP task, the value of zero means nothing */
-  pid_t	pid;
-  int	pipid;
-  /* exit code, this value is set when the PiP task */
-  /* gets PIP_GDBIF_STATUS_TERMINATED */
-  volatile int	exit_code;
-  /* pip task exec mode */
-  enum pip_task_exec_mode	exec_mode;
-  /* task status, this is set by PiP lib */
-  enum pip_task_status		status;
-  /* the variable(s) below are set/reset by GDB */
-  enum pip_gdb_status		gdb_status;
+/* make this independent from <pip.h> */
+enum pip_gdbif_pipid {
+  PIP_GDBIF_PIPID_ROOT		= -1,
+  PIP_GDBIF_PIPID_ANY		= -2
 };
 
-typedef void(*pip_gdbif_hook_t)(struct pip_gdbif_task*);
-
-struct pip_gdbif_root {
-  /* hook function address, these addresses are set */
-  /* when the PiP task gets PIP_GDBIF_STATUS_LOADED */
-  pip_gdbif_hook_t	hook_before_main;
-  pip_gdbif_hook_t	hook_after_main;
-
-  pip_spinlock_t	lock_free; /* lock for task_free */
-  PIP_SLIST_HEAD(, pip_gdbif_task) task_free;
-
-  pip_spinlock_t	lock_root; /* lock for task_root */
-  PIP_HCIRCLEQ_HEAD(pip_gdbif_task) task_root;
-  /* task_root == tasks[PIP_GDBIF_PIPID_ROOT], although it's not recommended */
-
-  struct pip_gdbif_task	tasks[];
+enum pip_task_status {
+  PIP_GDBIF_STATUS_NULL		= 0, /* just to make sure, there is nothing in this struct and invalid to access */
+  PIP_GDBIF_STATUS_CREATED	= 1, /* just after pip_spawn() is called and this structure is created */
+  /* Note: the order of state transition of the next two depends on implementation (option) */
+  /* do to rely on the order of the next two. */
+  PIP_GDBIF_STATUS_LOADED	= 2, /* just after calling dlmopen */
+  PIP_GDBIF_STATUS_SPAWNED	= 3, /* just after calling pthread_create() or clone() */
+  PIP_GDBIF_STATUS_TERMINATED	= 4  /* when the task is about to die (killed) */
 };
 
-#endif /* _pip_gdbif_h_ */
+/* gdb -> libpip */
+enum pip_gdb_status {
+  PIP_GDBIF_GDB_DETACHED	= 0, /* this structure can be freed */
+  PIP_GDBIF_GDB_ATTACHED	= 1  /* gdb is using this, cannot be freed */
+};
+
+enum pip_task_exec_mode {	/* One of the value (except NULL) is set when this structure is created */
+  /* and the value is left unchanged until the structure is put on the free list */
+  PIP_GDBIF_EXMODE_NULL		= 0,
+  PIP_GDBIF_EXMODE_PROCESS	= 1,
+  PIP_GDBIF_EXMODE_THREAD	= 2
+};
+
+#define PIP_GDBIF_ROOT_VARNAME		"pip_gdbif_root"
+
+#endif
