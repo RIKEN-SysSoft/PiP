a1f6eab59da8f1d106080b5749d039692e8fdd45
ahori@riken.jp <ahori@riken.jp>
2020-03-24 21:17:21 +0900

    forgot to add them

---
+++ include/pip_gdbif_func.h
@@ -0,0 +1,48 @@
+/*
+  * $RIKEN_copyright: 2018 Riken Center for Computational Sceience,
+  * 	  System Software Devlopment Team. All rights researved$
+  * $PIP_VERSION: Version 1.0$
+  * $PIP_license: <Simplified BSD License>
+  * Redistribution and use in source and binary forms, with or without
+  * modification, are permitted provided that the following conditions are
+  * met:
+  *
+  * 1. Redistributions of source code must retain the above copyright
+  *    notice, this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright
+  *    notice, this list of conditions and the following disclaimer in the
+  *    documentation and/or other materials provided with the distribution.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+  * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+  * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+  * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  * The views and conclusions contained in the software and documentation
+  * are those of the authors and should not be interpreted as representing
+  * official policies, either expressed or implied, of the PiP project.$
+*/
+/*
+  * Written by Atsushi HORI <ahori@riken.jp>
+*/
+
+#ifndef _pip_gdbif_func_h_
+#define _pip_gdbif_func_h_
+
+void pip_gdbif_load( pip_task_internal_t* ) PIP_PRIVATE;
+void pip_gdbif_exit( pip_task_internal_t*, int ) PIP_PRIVATE;
+void pip_gdbif_task_commit( pip_task_internal_t* ) PIP_PRIVATE;
+void pip_gdbif_task_new( pip_task_internal_t* ) PIP_PRIVATE;
+void pip_gdbif_initialize_root( int ) PIP_PRIVATE;
+void pip_gdbif_finalize_task( pip_task_internal_t* ) PIP_PRIVATE;
+void pip_gdbif_hook_before( pip_task_internal_t* ) PIP_PRIVATE;
+void pip_gdbif_hook_after( pip_task_internal_t* ) PIP_PRIVATE;
+
+#endif /* _pip_gdbif_func_h_ */
