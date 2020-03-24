3f264675f3f0d0051673e6e5fe5607c8f531dda9
ahori@riken.jp <ahori@riken.jp>
2020-03-24 17:25:31 +0900

    Fixing PiP-gdb bug and backporting some features from pip-2-blt branch         Fix PiP-gdb bug (unable to find inferiors when PiP root is not PIE)         Fix piprun bug (raising SIGSEGV) 	Adding pips command to show or kill all PiP root and tasks

--- include/pip_internal.h
+++ include/pip_internal.h
@@ -6,13 +6,13 @@
   * Redistribution and use in source and binary forms, with or without
   * modification, are permitted provided that the following conditions are
   * met:
-  * 
+  *
   * 1. Redistributions of source code must retain the above copyright
   *    notice, this list of conditions and the following disclaimer.
   * 2. Redistributions in binary form must reproduce the above copyright
-  *    notice, this list of conditions and the following disclaimer in the 
+  *    notice, this list of conditions and the following disclaimer in the
   *    documentation and/or other materials provided with the distribution.
-  * 
+  *
   * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
   * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
   * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -24,7 +24,7 @@
   * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
   * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
   * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  * 
+  *
   * The views and conclusions contained in the software and documentation
   * are those of the authors and should not be interpreted as representing
   * official policies, either expressed or implied, of the PiP project.$
@@ -51,7 +51,7 @@
 #include <pip_debug.h>
 
 /** ULP supported version **/
-#define PIP_BASE_VERSION	(0x2000U)
+#define PIP_BASE_VERSION	(0x2100U)
 /** The very first stable release version
 #define PIP_BASE_VERSION	(0x1000U)
 **/
