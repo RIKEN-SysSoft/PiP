00af91b97fc429729e27d7b41e4160756e13d344
ahori@riken.jp <ahori@riken.jp>
2020-08-06 11:07:45 +0900

    Change package name (pip -> process-in-process)

--- include/config.h
+++ include/config.h
@@ -5,13 +5,13 @@
 #define PACKAGE_BUGREPORT "pip@ml.riken.jp"
 
 /* Define to the full name of this package. */
-#define PACKAGE_NAME "pip"
+#define PACKAGE_NAME "Process-in-Processs"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "pip 3.0.0"
+#define PACKAGE_STRING "Process-in-Processs 3.0.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "pip"
+#define PACKAGE_TARNAME "proc-in-proc"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL "https://github.com/RIKEN-SysSoft/PiP"
