06a40044882e0974234200593ed796225e0bc169
ahori@riken.jp <ahori@riken.jp>
2020-09-25 16:42:37 +0900

    remove build.h

--- lib/build.h
+++
@@ -1,4 +0,0 @@
-#define BUILD_DATE "Fri Sep 25 07:41:38 UTC 2020"
-#define COMMIT_HASH "5aee835deaeb515f1ce0106f7b2b1d8adf823463"
-#define BUILD_OS "Linux 3.10.0-1062.12.1.el7.x86_64 #1 SMP Tue Feb 4 23:02:59 UTC 2020"
-#define BUILD_CC "gcc (GCC) 4.8.5 20150623 (Red Hat 4.8.5-39)"
