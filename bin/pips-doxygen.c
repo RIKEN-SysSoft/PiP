399ccf673fdb0bfe868d544e17b7eeb1ee0246ce
ahori@riken.jp <ahori@riken.jp>
2020-11-18 17:27:51 +0900

    Update README.md and RELEASE_NOTE

--- bin/pips-doxygen.c
+++ bin/pips-doxygen.c
@@ -2,14 +2,14 @@
  * $PIP_license: <Simplified BSD License>
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     Redistributions of source code must retain the above copyright notice,
  *     this list of conditions and the following disclaimer.
- * 
+ *
  *     Redistributions in binary form must reproduce the above copyright notice,
  *     this list of conditions and the following disclaimer in the documentation
  *     and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -36,7 +36,7 @@
 \brief List or kill running PiP tasks
 
 \synopsis
-pips [a][u][x] [PIP-OPTIONS] [-] [PATTERN ..]
+pips [a][u][x] [PIPS-OPTIONS] [-] [PATTERN ..]
 
 \param a|u|x similar to the aux options of the Linux ps command
 \param \-\-root List PiP root(s)
