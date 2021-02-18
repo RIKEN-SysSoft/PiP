2d93cc8dbb9fb1cef2388e7f1fce1477a4c5b69a
ahori@riken.jp <ahori@riken.jp>
2021-02-18 13:41:49 +0900

    restructuring sample program directory

--- examples/hello.c
+++ examples/hello.c
@@ -35,57 +35,26 @@
 
 #include <pip/pip.h>
 
-#define NDATA		1000000
-
-struct dat {
-  pip_barrier_t	barrier;
-  double	data[NDATA];
-} data;
-
-double output = 0.0;
+int pipid, ntasks;
 
 int main( int argc, char **argv ) {
-  void *export = (void*) &data;
-  int  i, ntasks, pipid;
 
-  ntasks = 8;
-  pip_init( &pipid, &ntasks, (void*) &export, 0 );
-  if( pipid == PIP_PIPID_ROOT ) {
-    pip_barrier_init( &data.barrier, ntasks + 1 );
-    for( i=0; i<NDATA; i++ ) data.data[i] = (double) i;
-    for( i=0; i<ntasks; i++ ) {
-      pipid = i;
-      pip_spawn( argv[0], argv, NULL, i, &pipid, NULL, NULL, NULL );
-    }
-    pip_barrier_wait( &data.barrier );
-    for( i=0; i<ntasks; i++ ) {
-      void *import;
-      pip_import( i, &import );
-      /* gather individual result */
-      output += *((double*)(import));
-    }
-    pip_barrier_wait( &data.barrier );
-    for( i=0; i<ntasks; i++ ) pip_wait( i, NULL );
-    printf( "output = %g\n", output );
+  ntasks = 10;
+  pip_init( &pipid, &ntasks, NULL, 0 );
 
-  } else {	/* PIP child task */
-    struct dat* import = (struct dat*) export;
-    double *input = import->data;
-    int start, end;
+  if( pipid == PIP_PIPID_ROOT ) { // PIP Root process
+    pipid = PIP_PIPID_ANY;
+    printf( "<R> &pipid: %p\n", &pipid );
+    pip_spawn( argv[0], argv, NULL, PIP_CPUCORE_ASIS, &pipid,
+	       NULL, NULL, NULL );
+    pip_wait( pipid, NULL );
 
-    start = ( NDATA / ntasks ) * pipid;
-    end = start + ( NDATA / ntasks );
-    printf( "PIPID:%d  data[%d-%d]\n", pipid, start, end-1 );
-    for( i=start; i<end; i++ ) {
-      /* do computation on imported data */
-      output += input[i];
-    }
-    /* note that any stack variables can also be exported */
-    pip_export( (void*) &output );
-
-    pip_barrier_wait( &import->barrier );
-    /* here, the main task gathers child data */
-    pip_barrier_wait( &import->barrier );
+  } else {			// PIP child task
+#ifdef PRINT_MAPS
+    pip_print_maps();
+#endif
+    printf( "<%d> &pipid: %p\n", pipid, &pipid );
+    printf( "<%d> Hello, I am fine !!\n", pipid );
   }
   pip_fin();
   return 0;
